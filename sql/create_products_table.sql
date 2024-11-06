89502db57e7f32ea92375b4d1999457cd8eeb4ee
Barbaros Yahya <barbaros.acet@sabanciuniv.edu>
2024-11-06 21:29:20 +0300

    changed the product model according to the objectives

--- sql/create_products_table.sql
+++ sql/create_products_table.sql
@@ -2,8 +2,12 @@
 -- Product Model Table
 CREATE TABLE IF NOT EXISTS products (
     productID SERIAL PRIMARY KEY,
-    productName VARCHAR(255)  NOT NULL,
-    category VARCHAR(255) NOT NULL,           
+    serialNumber SERIAL UNIQUE,
+    productName VARCHAR(50)  NOT NULL,
+    productModel VARCHAR(50)  NOT NULL,
+    description VARCHAR(255)  NOT NULL,
+    distributerInfo VARCHAR(255)  NOT NULL,
+    warranty VARCHAR(50) NOT NULL,          
     price NUMERIC(10,2) NOT NULL CHECK (price > 0),
     stock INT NOT NULL CHECK (stock >=0 )
 );
