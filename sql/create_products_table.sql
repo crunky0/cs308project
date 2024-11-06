a82ecc576872da3a1bf0499f5239a6838c3a1e6e
Barbaros Yahya <barbaros.acet@sabanciuniv.edu>
2024-11-06 21:18:02 +0300

    creation of product sql model

---
+++ sql/create_products_table.sql
@@ -0,0 +1,9 @@
+
+-- Product Model Table
+CREATE TABLE IF NOT EXISTS products (
+    productID SERIAL PRIMARY KEY,
+    productName VARCHAR(255)  NOT NULL,
+    category VARCHAR(255) NOT NULL,           
+    price NUMERIC(10,2) NOT NULL CHECK (price > 0),
+    stock INT NOT NULL CHECK (stock >=0 )
+);
