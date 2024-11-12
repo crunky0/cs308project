ced5d33462785a2631c023fb7900caf728cc68ec
Barbaros Yahya <barbaros.acet@sabanciuniv.edu>
2024-11-12 15:55:16 +0300

    Create db.py

---
+++ db.py
@@ -0,0 +1,10 @@
+import os
+from databases import Database
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+# Create the database connection instance
+DATABASE_URL = os.getenv("DATABASE_URL")
+database = Database(DATABASE_URL)
