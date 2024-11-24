d091bab05e6f94a79bc7746288b121c937813642
Barbaros Yahya <barbaros.acet@sabanciuniv.edu>
2024-11-24 18:32:45 +0300

    Created router for search endpoints. Created workspace for implementation of work

---
+++ main.py
@@ -0,0 +1,37 @@
+
+import os
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from db import database
+
+from stock_endpoints import router as stock_router  # Import the router
+from user_endpoints import router as user_router  # Import the router
+from rating_endpoints import router as rating_router
+from categorysearch_endpoints import router as category_search_router
+from card_endpoints import router as card_router
+from invoice_endpoints import router as invoice_router
+from search_endpoints import router as search_router
+
+
+# FastAPI app initialization with lifespan context
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup logic
+    if not database.is_connected:
+        await database.connect()
+    yield  # This yields control to the application during its lifespan
+    # Shutdown logic
+    if database.is_connected:
+        await database.disconnect()
+
+app = FastAPI(lifespan=lifespan)
+
+# Include the user router
+
+app.include_router(stock_router)
+app.include_router(user_router)
+app.include_router(rating_router)
+app.include_router(category_search_router)
+app.include_router(card_router)
+app.include_router(invoice_router)
+app.include_router(search_router)
